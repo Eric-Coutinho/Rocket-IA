fbd5f5b87ba9f0125f075c747d8be4b18281375b
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-03-04 13:29:23 -0300

    Fixed tree and treenode

--- Collections/Tree.cs
+++ Collections/Tree.cs
@@ -3,15 +3,31 @@ namespace Desafio_Foguete.Collections;
 public class Tree<T>
 {
     public TreeNode<T> Root { get; set; }
-    public List<TreeNode<T>> Children => Root.Children;
-    public Tree(TreeNode<T> root) => Root = root;
+    public List<TreeNode<T>> Branches => Root.Children;
+    public Tree(TreeNode<T> root = null!)
+    {
+        Root = root;
+    }
     public Tree(T value) => Root = new TreeNode<T>(value);
 
-    public void AddBranch(Tree<T> branch)
-        => Root.AddChild(branch.Root);
+    public Tree<T> AddBranch(TreeNode<T> branch)
+    {
+        Root.AddChild(branch);
+        branch.Parent = Root;
 
-    public void RemoveBranch(Tree<T> branch)
-        => Root.RemoveChild(branch.Root);
+        return this;
+    }
 
-    
+    public Tree<T> AddBranch(Tree<T> branch)
+    {
+        Root.AddChild(branch.Root);
+        branch.Root.Parent = Root;
+
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return Root.ToString();
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,30 +1,51 @@
 ﻿using AIContinuous;
 using Desafio_Foguete.Collections;
 
+var tree = BuildTree();
+Console.WriteLine(tree);
+return;
 
-// Tree 1 - root: 50
-var node = new TreeNode<int>(6);
-node = new TreeNode<int>(21, children : new List<TreeNode<int>> { node });
-var node2 = new TreeNode<int>(45);
-node = new TreeNode<int>(12, children : new List<TreeNode<int>> { node, node2 });
-node = new TreeNode<int>(50, children : new List<TreeNode<int>> { node });
+Tree<int> BuildTree()
+{
+    // Tree 1 (root: 50)
+    var node = new TreeNode<int>(6);
+    node = new TreeNode<int>(21, children: new List<TreeNode<int>> { node });
+    var node2 = new TreeNode<int>(45);
+    node = new TreeNode<int>(12, children: new List<TreeNode<int>> { node, node2 });
+    node = new TreeNode<int>(50, children: new List<TreeNode<int>> { node });
 
-var tree1 = new Tree<int>(node);
+    var tree1 = new Tree<int>(node);
 
-// Tree 2 - root: 1
-var root = new TreeNode<int>(1);
-root.AddChild(new TreeNode<int>(70))
-    .AddChild(new TreeNode<int>(61));
+    // Tree 2 (root: 1)
+    var root = new TreeNode<int>(1)
+               .AddChild(new TreeNode<int>(70))
+               .AddChild(new TreeNode<int>(61));
 
-var tree2 = new Tree<int>(root);
+    var tree2 = new Tree<int>(root);
 
-// Tree 3 - root: 150
-root = new TreeNode<int>(150)
-    .AddChild(new TreeNode<int>(30)
-            .AddChild(new TreeNode<int>(96))
-            .AddChild(new TreeNode<int>(9))
-            )
-    .AddChild(new TreeNode<int>(5))
-    .AddChild(new TreeNode<int>(11));
+    // Tree 3 (root: 30)
+    root = new TreeNode<int>(30)
+           .AddChild(new TreeNode<int>(96))
+           .AddChild(new TreeNode<int>(9));
 
+    var tree3 = new Tree<int>(root);
+
+    // Tree4 (root: 150)
+    root = new TreeNode<int>(150)
+           .AddChild(tree3.Root)
+           .AddChild(new TreeNode<int>(5))
+           .AddChild(new TreeNode<int>(11));
+
+    var tree4 = new Tree<int>(root);
+
+    // Tree 5 (root: 100)
+    root = new TreeNode<int>(100)
+           .AddChild(tree1.Root)
+           .AddChild(tree2.Root)
+           .AddChild(tree4.Root);
+
+    var tree5 = new Tree<int>(root);
+
+    return tree5;
+}
 
